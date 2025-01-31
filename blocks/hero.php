e9d837d078e36e4c3a470bf2b9b37e8645d22c7e
MaximusMixik <maximus.mixik@gmail.com>
2025-01-31 13:17:38 +0200

    update(Block: Hero)

--- blocks/hero.php
+++ blocks/hero.php
@@ -9,18 +9,20 @@ $title = $heading['title'];
 $titleTag = $heading['title_tag'];
 $content = $heading['content'];
 
+$content_position =  get_sub_field('hero_content_position');
+
 $background = get_sub_field('hero_image');
 
 if(!$heading)  return ;
 
 ?>
 
-<section class="hero ">
+<section class="hero <?php echo "hero--" . $content_position ?>">
 	<?php if($background): ?>
 <!-- 		<img class="hero__background" <?php acf_image_attrs($background) ?> class="skip-lazy" fetchpriority="high"> -->
 		<img fetchpriority="high" <?php awesome_acf_responsive_image($background['id'],'large','1920px'); ?> alt="<?php echo $banner_image['alt'] ?: $banner_image['title']; ?>" data-no-lazy="1" class="hero__background no-lazy skip-lazy" width="1920" height="615">
 	<?php endif; ?>
-	<div class="hero__container | container">
+	<div class="hero__container | container ">
 		<?php render_heading_block($label, $title, $titleTag, $content, 'hero__content'); ?>
 	</div>
 </section>
