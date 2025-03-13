df0060986bf695d067282f33910e0012f87a06d6
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2025-03-13 15:41:24 +0100

    Feat:  update schedule page

--- blocks/schedule.php
+++ blocks/schedule.php
@@ -12,7 +12,7 @@ $content = $content['content'];
 		<section class="schedule"> 
 				<div class="schedule__container | container row">
 					<div class="schedule__about | col-12 col-md-7">
-							<?php render_content_block($label, $content, 'schedule__content');?>
+						<?php render_content_block($label, $content); ?>
 					</div>
 					<div class="schedule__sidebar  | col-12 col-md-5">
 						<aside class="sidebar  | animate fade-up">
@@ -55,5 +55,68 @@ $content = $content['content'];
 					</div>
 				</div>
 		</section>
+<style>
+@media (min-width: 768px) {
+    .wcs-timetable__compact-list .wcs-day .wcs-timetable__classes::before {
+        border-left: 1px solid #80BB3D4D;
+    }
+}
+
+@media (min-width: 768px) {
+    .wcs-timetable__compact-list .wcs-class__time {
+        width: 100%;
+				text-align: left;
+				padding: 0 2vh;
+}	
+	.wcs-timetable__compact-list .wcs-class {
+		flex-wrap: wrap;
+	}
+	
+	.wcs-timetable__compact-list .wcs-class {
+		padding-bottom: 16px;
+		padding-top: 16px;
+	}
+	.wcs-class {
+	position: relative;
+}
+
+.wcs-class::before,
+.wcs-timetable__compact-list .wcs-class:not(.wcs-class--canceled):hover::before{
+	content: "";
+	position: absolute;
+	left: -6px;
+	top: -6px;
+	width: 11px;
+	height: 11px;
+	background-color: #80BB3D;
+	border-radius: 50%;
+	opacity: 1;
+}
+}
+
+
+
+
+.wcs-timetable__compact-list .wcs-class--visible::after {
+	display: none;
+}
+
+.wcs-timetable__compact-list .wcs-day .wcs-timetable__classes::before {
+	border-top: 0;
+}
+
+.wcs-class__title {
+	margin-bottom: 16px !important;
+}
+
+.wcs-day__date {
+	color: #80BB3D;
+	font-size: 26px;
+	font-weight: 600;
+}
+
+.wcs-timetable__compact-list .wcs-day--visible:not(:last-child)::after {
+	opacity: 0;
+}</style>
 
 <?php ?>
